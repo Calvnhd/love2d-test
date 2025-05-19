8f736df9b17878fb31f262cd3199d6f9341aba2f
Calvin Davidson <82812675+Calvnhd@users.noreply.github.com>
2025-05-20 00:56:57 +1000

    speed running to the end

--- src/main.lua
+++ src/main.lua
@@ -1,16 +1,6 @@
-local debugMode = false
-local logToFile
-local debugPrint
-
-local function takeCard(hand)
-    table.insert(hand, table.remove(deck, love.math.random(#deck))) -- # operator returns the number of elements in the table
-end
-
--- initialization function
 function love.load()
-    logToFile("love.load start")
+    love.graphics.setBackgroundColor(1, 1, 1)
 
-    -- load image files
     images = {}
     for nameIndex, name in ipairs({
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
@@ -22,142 +12,315 @@ function love.load()
         images[name] = love.graphics.newImage('images/'..name..'.png')
     end
 
-    -- init deck
-    deck = {}
-    for suitIndex, suitValue in ipairs({"hearts", "diamonds", "clubs", "spades"}) do
-        for rankIndex, rankValue in ipairs({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}) do
-            table.insert(deck, {
-                suit = suitValue,
-                rank = rankValue
-            })
-            debugPrint('suit: ' .. suitValue .. ', rank: ' .. rankValue)
-        end
+    function takeCard(hand)
+        table.insert(hand, table.remove(deck, love.math.random(#deck)))
     end
 
-    -- deal starting hands
-    playerHand = {}
-    takeCard(playerHand)
-    takeCard(playerHand)
-    dealerHand = {}
-    takeCard(dealerHand)
-    takeCard(dealerHand)
-
-    -- feels weird putting this here but hey
-    roundOver = false
-
-    -- I don't like putting functions in functions but okay
     function getTotal(hand)
         local total = 0
-        local bHasAce = false
+        local hasAce = false
+
         for cardIndex, card in ipairs(hand) do
             if card.rank > 10 then
                 total = total + 10
             else
                 total = total + card.rank
             end
+
             if card.rank == 1 then
-                bHasAce = true
+                hasAce = true
             end
         end
-        if bHasAce and total <= 11 then
+
+        if hasAce and total <= 11 then
             total = total + 10
         end
+
         return total
     end
 
-    logToFile("love.load complete")
+    local buttonY = 230
+    local buttonHeight = 25
+    local textOffsetY = 6
+
+    buttonHit = {
+        x = 10,
+        y = buttonY,
+        width = 53,
+        height = buttonHeight,
+        text = 'Hit!',
+        textOffsetX = 16,
+        textOffsetY = textOffsetY,
+    }
+
+    buttonStand = {
+        x = 70,
+        y = buttonY,
+        width = 53,
+        height = buttonHeight,
+        text = 'Stand',
+        textOffsetX = 8,
+        textOffsetY = textOffsetY,
+    }
+
+    buttonPlayAgain = {
+        x = 10,
+        y = buttonY,
+        width = 113,
+        height = buttonHeight,
+        text = 'Play again',
+        textOffsetX = 24,
+        textOffsetY = textOffsetY,
+    }
+
+    function isMouseInButton(button)
+        return love.mouse.getX() >= button.x
+        and love.mouse.getX() < button.x + button.width
+        and love.mouse.getY() >= button.y
+        and love.mouse.getY() < button.y + button.height
+    end
+
+    function reset()
+        deck = {}
+        for suitIndex, suit in ipairs({'club', 'diamond', 'heart', 'spade'}) do
+            for rank = 1, 13 do
+                table.insert(deck, {suit = suit, rank = rank})
+            end
+        end
+
+        playerHand = {}
+        takeCard(playerHand)
+        takeCard(playerHand)
+
+        dealerHand = {}
+        takeCard(dealerHand)
+        takeCard(dealerHand)
+
+        roundOver = false
+    end
+
+    reset()
 end
 
--- tick function
-function love.update(dt)
-end
-
--- draw function
-function love.draw()
-
-    -- building the string (output) to display
-    local output = {}
-
-    -- add player info 
-    table.insert(output, "--- Player Hand ---")
-    for cardIndex, card in ipairs(playerHand) do
-        table.insert(output, "suit: " .. card.suit .. ", rank: " .. card.rank)
-    end
-    table.insert(output, "Total: " .. getTotal(playerHand))
-
-    -- new line
-    table.insert(output, "")
-
-    -- add Dealer info
-    table.insert(output, "--- Dealer Hand ---")
-    for cardIndex, card in ipairs(dealerHand) do
-        if not roundOver and cardIndex == 1 then
-            table.insert(output, '(Card hidden)')
-        else
-            table.insert(output, "suit: " .. card.suit .. ", rank: " .. card.rank)
-        end
-    end
-    if roundOver then
-        table.insert(output, 'Total: '..getTotal(dealerHand))
-    else
-        table.insert(output, 'Total: ?')
-    end
-
-    -- round over info
-    if roundOver then
-        table.insert(output, '')
-
-        local function hasHandWon(thisHand, otherHand)
-            return getTotal(thisHand) <= 21 and (getTotal(otherHand) > 21 or getTotal(thisHand) > getTotal(otherHand))
-        end
-
-        if hasHandWon(playerHand, dealerHand) then
-            table.insert(output, 'Player wins')
-        elseif hasHandWon(dealerHand, playerHand) then
-            table.insert(output, 'Dealer wins')
-        else
-            table.insert(output, 'Draw')
-        end
-    end
-
-    -- print resulting string to a table
-    love.graphics.print(table.concat(output, "\n"), 15, 15)
-
-end
-
--- input
-function love.keypressed(key)
+function love.mousereleased()
     if not roundOver then
-        if key == 'h' then
+        if isMouseInButton(buttonHit) then
             takeCard(playerHand)
             if getTotal(playerHand) >= 21 then
                 roundOver = true
             end
-        elseif key == 's' then
+        elseif isMouseInButton(buttonStand) then
             roundOver = true
         end
+
         if roundOver then
             while getTotal(dealerHand) < 17 do
                 takeCard(dealerHand)
             end
         end
-    else
-        love.load()
+    elseif isMouseInButton(buttonPlayAgain) then
+        reset()
     end
 end
 
--- logs message input into a file
-logToFile = function(message)
-    local timestamp = os.date("[%Y-%m-%d %H:%M:%S]")
-    local file = io.open("blackjack-log.txt", "a")
-    file:write(timestamp .. " " .. message .. "\n")
-    file:close()
-    debugPrint(message)
-end
+function love.draw()
+    local function drawCard(card, x, y)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.draw(images.card, x, y)
 
--- prints message to console
-debugPrint = function(message)
-    if debugMode then
-        print(message)
+        if card.suit == 'heart' or card.suit == 'diamond' then
+            love.graphics.setColor(.89, .06, .39)
+        else
+            love.graphics.setColor(.2, .2, .2)
+        end
+
+        local cardWidth = 53
+        local cardHeight = 73
+
+        local function drawCorner(image, offsetX, offsetY)
+            love.graphics.draw(
+                image,
+                x + offsetX,
+                y + offsetY
+            )
+            love.graphics.draw(
+                image,
+                x + cardWidth - offsetX,
+                y + cardHeight - offsetY,
+                0,
+                -1
+            )
+        end
+
+        drawCorner(images[card.rank], 3, 4)
+        drawCorner(images['mini_'..card.suit], 3, 14)
+
+        if card.rank > 10 then
+            local faceImage
+
+            if card.rank == 11 then
+                faceImage = images.face_jack
+            elseif card.rank == 12 then
+                faceImage = images.face_queen
+            elseif card.rank == 13 then
+                faceImage = images.face_king
+            end
+
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.draw(faceImage, x + 12, y + 11)
+        else
+            local function drawPip(offsetX, offsetY, mirrorX, mirrorY)
+                local pipImage = images['pip_'..card.suit]
+                local pipWidth = 11
+
+                love.graphics.draw(
+                    pipImage,
+                    x + offsetX,
+                    y + offsetY
+                )
+                if mirrorX then
+                    love.graphics.draw(
+                        pipImage,
+                        x + cardWidth - offsetX - pipWidth,
+                        y + offsetY
+                    )
+                end
+                if mirrorY then
+                    love.graphics.draw(
+                        pipImage,
+                        x + offsetX + pipWidth,
+                        y + cardHeight - offsetY,
+                        0,
+                        -1
+                    )
+                end
+                if mirrorX and mirrorY then
+                    love.graphics.draw(
+                        pipImage,
+                        x + cardWidth - offsetX,
+                        y + cardHeight - offsetY,
+                        0,
+                        -1
+                    )
+                end
+            end
+
+            local xLeft = 11
+            local xMid = 21
+            local yTop = 7
+            local yThird = 19
+            local yQtr = 23
+            local yMid = 31
+
+            if card.rank == 1 then
+                drawPip(xMid, yMid)
+
+            elseif card.rank == 2 then
+                drawPip(xMid, yTop, false, true)
+
+            elseif card.rank == 3 then
+                drawPip(xMid, yTop, false, true)
+                drawPip(xMid, yMid)
+
+            elseif card.rank == 4 then
+                drawPip(xLeft, yTop, true, true)
+
+            elseif card.rank == 5 then
+                drawPip(xLeft, yTop, true, true)
+                drawPip(xMid, yMid)
+
+            elseif card.rank == 6 then
+                drawPip(xLeft, yTop, true, true)
+                drawPip(xLeft, yMid, true)
+
+            elseif card.rank == 7 then
+                drawPip(xLeft, yTop, true, true)
+                drawPip(xLeft, yMid, true)
+                drawPip(xMid, yThird)
+
+            elseif card.rank == 8 then
+                drawPip(xLeft, yTop, true, true)
+                drawPip(xLeft, yMid, true)
+                drawPip(xMid, yThird, false, true)
+
+            elseif card.rank == 9 then
+                drawPip(xLeft, yTop, true, true)
+                drawPip(xLeft, yQtr, true, true)
+                drawPip(xMid, yMid)
+
+            elseif card.rank == 10 then
+                drawPip(xLeft, yTop, true, true)
+                drawPip(xLeft, yQtr, true, true)
+                drawPip(xMid, 16, false, true)
+            end
+        end
+    end
+
+    local cardSpacing = 60
+    local marginX = 10
+
+    for cardIndex, card in ipairs(dealerHand) do
+        local dealerMarginY = 30
+        if not roundOver and cardIndex == 1 then
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.draw(images.card_face_down, marginX, dealerMarginY)
+        else
+            drawCard(card, ((cardIndex - 1) * cardSpacing) + marginX, dealerMarginY)
+        end
+    end
+
+    for cardIndex, card in ipairs(playerHand) do
+        drawCard(card, ((cardIndex - 1) * cardSpacing) + marginX, 140)
+    end
+
+    love.graphics.setColor(0, 0, 0)
+
+    if roundOver then
+        love.graphics.print('Total: '..getTotal(dealerHand), marginX, 10)
+    else
+        love.graphics.print('Total: ?', marginX, 10)
+    end
+
+    love.graphics.print('Total: '..getTotal(playerHand), marginX, 120)
+
+    if roundOver then
+        local function hasHandWon(thisHand, otherHand)
+            return getTotal(thisHand) <= 21
+            and (
+                getTotal(otherHand) > 21
+                or getTotal(thisHand) > getTotal(otherHand)
+            )
+        end
+
+        local function drawWinner(message)
+            love.graphics.print(message, marginX, 268)
+        end
+
+        if hasHandWon(playerHand, dealerHand) then
+            drawWinner('Player wins')
+        elseif hasHandWon(dealerHand, playerHand) then
+            drawWinner('Dealer wins')
+        else
+            drawWinner('Draw')
+        end
+    end
+
+    local function drawButton(button)
+        if isMouseInButton(button) then
+            love.graphics.setColor(1, .8, .3)
+        else
+            love.graphics.setColor(1, .5, .2)
+        end
+
+        love.graphics.rectangle('fill', button.x, button.y, button.width, button.height)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.print(button.text, button.x + button.textOffsetX, button.y + button.textOffsetY)
+    end
+
+    if roundOver then
+        drawButton(buttonPlayAgain)
+    else
+        drawButton(buttonHit)
+        drawButton(buttonStand)
     end
 end
